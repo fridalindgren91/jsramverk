ae780f1edea068e6497d544170a123c7cc7bb12f
frlg18 <fridalindgren_@outlook.com>
2019-09-08 22:57:30 +0100

    Kmom01 klar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,36 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { MeComponent } from './me/me.component';
+import { Kmom01Component } from './reports/kmom01/kmom01.component';
+
+const appRoutes: Routes = [
+  { path: 'me', component: MeComponent },
+  { path: '',
+    redirectTo: '/me',
+    pathMatch: 'full'
+  },
+  { path: 'reports/week/1', component: Kmom01Component }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MeComponent,
+    Kmom01Component
   ],
   imports: [
-    BrowserModule
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    ),
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
